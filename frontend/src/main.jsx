2333cfd9709ea17ae2857d4083ae015aa02e8f12
AmulyaJain2004 <jainamulyawin@gmail.com>
2025-11-13 07:44:57 +0530

    update code

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { UserProvider } from './context/userContext'
-import { ChatProvider } from './context/ChatContext'
-import { SpeedInsights } from "@vercel/speed-insights/react"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { UserProvider } from "./context/userContext";
+import { ChatProvider } from "./context/ChatContext";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <UserProvider>
       <ChatProvider>
@@ -14,5 +14,5 @@ createRoot(document.getElementById('root')).render(
         <SpeedInsights />
       </ChatProvider>
     </UserProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
